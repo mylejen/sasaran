48ced3fd1efdf697c02849a46d02a419870d9485
MYLEJEN <95838839+mylejen@users.noreply.github.com>
2021-12-17 19:40:32 +0800

    Update v1.js

--- v1.js
+++ v1.js
@@ -1,4 +1,5 @@
 var customDonationCurrentValue = Number(PageData.salesStats.total_amount_all);
+var customDonationTargetValue = 200000; // Please customize your target value as you see fit
 var customDonationPercentage = (customDonationCurrentValue / customDonationTargetValue) * 100;
 $("#custom-donation-current-value").html(customDonationCurrentValue.toLocaleString(undefined, { minimumFractionDigits: 2 }));
 $("#custom-donation-target-value").html(customDonationTargetValue.toLocaleString(undefined, { minimumFractionDigits: 2 }));
